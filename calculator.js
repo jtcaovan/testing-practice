67a65f1072318f4fb6b5b7df1400de159a13bf70
Justin Caovan <jtcaovan@gmail.com>
2021-06-17 22:57:02 -0700

    Add calculate test

---
+++ calculator.js
@@ -0,0 +1,10 @@
+function calculate(a, b) {
+    const calculator = {
+        'add': a + b,
+        'substract': a - b,
+        'multiply': a * b,
+        'divide': a / b
+    }
+}
+
+module.exports = calculate
